3245a1cebdff4420c2ab9cc5bef00e049e5c237e
deepakgla16 <deepaksharma972079@gmail.com>
2025-03-24 16:48:43 +0530

    Adding logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  url="../assets/download.avif"
   
   ngOnInit(): void{
     this.title="hello from bridgeLabz"
